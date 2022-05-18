0b24cb22a9ff066eceb5db7b71a5bacef845f5d6
Aleksandr Filippov <alexander.filippov@iqtek.ru>
2022-05-18 17:06:06 +0600

    Fixed some bugs.

--- amocrm_asterisk_ng/scenario/impl/classic/event_handlers/RingingEventHandler.py
+++ amocrm_asterisk_ng/scenario/impl/classic/event_handlers/RingingEventHandler.py
@@ -32,10 +32,13 @@ class RingingEventHandler(IEventHandler):
 
     async def __call__(self, event: RingingEvent) -> None:
 
-        user_id = await self.__get_user_id_by_phone_query(
-            phone_number=event.called_phone_number,
-        )
-
+        try:
+            user_id = await self.__get_user_id_by_phone_query(
+                phone_number=event.called_phone_number,
+            )
+        except Exception:
+            # is not internal call
+            return
         if not await self.__is_internal_number_function(event.caller_phone_number):
             await self.__raise_card_command(
                 phone_number=event.caller_phone_number,
