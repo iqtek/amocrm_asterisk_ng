ce388254cf915bd9d3f613510d159b6fad968b0a
Aleksandr Filippov <alexander.filippov@iqtek.ru>
2022-12-14 17:43:02 +0600

    Added contact search by multiple numbers.

--- asterisk_ng/plugins/crm_system/amocrm/kernel/functions/functions/GetContactByPhoneQueryImpl.py
+++ asterisk_ng/plugins/crm_system/amocrm/kernel/functions/functions/GetContactByPhoneQueryImpl.py
@@ -1,3 +1,7 @@
+from re import search
+
+from typing import Collection
+
 from amocrm_api_client import AmoCrmApiClient
 from amocrm_api_client.make_amocrm_request.core import EntityNotFoundException
 from amocrm_api_client.models import Contact
@@ -11,6 +15,8 @@ __all__ = ["GetContactByPhoneQueryImpl"]
 
 class GetContactByPhoneQueryImpl(IGetContactByPhoneQuery):
 
+    __POSSIBLE_PREFIXES = ["+7", "8"]
+
     __slots__ = (
         "__amo_client",
     )
@@ -18,16 +24,30 @@ class GetContactByPhoneQueryImpl(IGetContactByPhoneQuery):
     def __init__(self, amo_client: AmoCrmApiClient) -> None:
         self.__amo_client = amo_client
 
+    def __get_possible_numbers(self, phone: str) -> Collection[str]:
+        results = search("(\\d{10})$", phone)
+
+        if results is None:
+            return phone,
+
+        return [prefix + results[0] for prefix in self.__POSSIBLE_PREFIXES]
+
     async def __call__(self, phone_number: str) -> CrmContact:
-        try:
-            contact: Contact = (await self.__amo_client.contacts.smart_redirect(phone_number))[0]
-        except EntityNotFoundException:
-            raise KeyError(f"Contact by phone: `{phone_number}`not found.")
-        else:
-            return CrmContact(
-                id=contact.id,
-                responsible_user_id=CrmUserId(id=contact.responsible_user_id),
-                name=contact.name,
-                first_name=contact.first_name,
-                last_name=contact.last_name,
-            )
+
+        possible_numbers = self.__get_possible_numbers(phone_number)
+
+        for possible_number in possible_numbers:
+            try:
+                contact: Contact = (await self.__amo_client.contacts.smart_redirect(possible_number))[0]
+            except EntityNotFoundException:
+                continue
+            else:
+                return CrmContact(
+                    id=contact.id,
+                    responsible_user_id=CrmUserId(id=contact.responsible_user_id),
+                    name=contact.name,
+                    first_name=contact.first_name,
+                    last_name=contact.last_name,
+                )
+
+        raise KeyError(f"Contact by phone: `{phone_number}`not found.")
