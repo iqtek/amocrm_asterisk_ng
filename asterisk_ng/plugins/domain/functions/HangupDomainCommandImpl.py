c1eadbba425d927c1eb08663688348455272779b
Aleksandr Filippov <alexander.filippov@iqtek.ru>
2023-01-31 12:05:20 +0600

    Added forced call termination im HangupDomainCommandImpl.py.

--- asterisk_ng/plugins/domain/functions/HangupDomainCommandImpl.py
+++ asterisk_ng/plugins/domain/functions/HangupDomainCommandImpl.py
@@ -1,5 +1,7 @@
 from typing import Mapping
+from typing import MutableMapping
 
+from asterisk_ng.interfaces import CallDomainModel
 from asterisk_ng.interfaces import CrmUserId
 from asterisk_ng.interfaces import IHangupDomainCommand
 from asterisk_ng.interfaces import IHangupTelephonyCommand
@@ -18,11 +20,14 @@ class HangupDomainCommandImpl(IHangupDomainCommand):
     def __init__(
         self,
         agent_id_to_phone_mapping: Mapping[CrmUserId, str],
+        active_calls: MutableMapping[CrmUserId, CallDomainModel],
         hangup_telephony_command: IHangupTelephonyCommand,
     ) -> None:
         self.__agent_id_to_phone_mapping = agent_id_to_phone_mapping
+        self.__active_calls = active_calls
         self.__hangup_telephony_command = hangup_telephony_command
 
     async def __call__(self, user_id: CrmUserId) -> None:
         agent_phone = self.__agent_id_to_phone_mapping[user_id]
+        self.__active_calls.pop(user_id, None)
         await self.__hangup_telephony_command(phone_number=agent_phone)
