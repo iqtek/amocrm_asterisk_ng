f1999db157db710e90ed8fccbfdec8ad1bf27521
ergnoore <alekxandr10112002@gmail.com>
2023-09-19 17:40:09 +0600

    Change version 3.0.2 -> 3.0.3

--- asterisk_ng/__init__.py
+++ asterisk_ng/__init__.py
@@ -7,7 +7,7 @@ __all__ = [
 ]
 
 __name__ = "AsteriskNG"
-__version__ = "3.0.2"
+__version__ = "3.0.3"
 __description__ = "Amocrm & Asterisk integration."
 __author__ = "Iqtek, LLC"
 __license__ = "MIT"
